e1b9d69eeda0b3cf58304a34e112b623d16b1742
Kavish Patel <kavish619@gmail.com>
2016-02-03 18:58:24 +0530

    Composite example

--- react-exp.jsx
+++ react-exp.jsx
@@ -1,34 +1,34 @@
 if (Meteor.isClient) {
-    LikeButton = React.createClass({
-        getInitialState() {
-            return {liked: true};
-        },
-        handleClick(event) {
-            this.setState({liked: !this.state.liked});
-        },
-        render() {
-            var text = this.state.liked ? 'liked' : 'haven\'t liked';
-            return ( <p onClick={this.handleClick}> You {text} this. Click to toggle. < /p> );
-        }
-    });
-
-    App = React.createClass({
-        render() {
-            return ( 
-                <div className="ui container">
-                <p>Hello, <input type="text" placeholder="Your name here" />!It is {this.props.date.toTimeString()}</p>
-                </div>
-            );
-        }
-    });
-
-    Meteor.startup(function() {
-        setInterval(function() {
-            ReactDOM.render(
-                <App date={new Date()} />,document.getElementById('render-target'));}, 1000);
-        ReactDOM.render(
-            <LikeButton /> ,document.getElementById('like-example')
+    Avatar = React.createClass({
+      render() {
+        return (
+          <div>
+            <ProfilePic username={this.props.username} />
+            <br/>
+            <ProfileLink username={this.props.username} />
+          </div>
         );
+      }
+    });
+
+    var ProfilePic = React.createClass({
+      render() {
+        return (
+          <img src={'https://graph.facebook.com/' + this.props.username + '/picture'} />
+        );
+      }
+    });
+
+    var ProfileLink = React.createClass({
+      render() {
+        return (
+          <a href={'https://www.facebook.com/' + this.props.username}>{this.props.username}</a>
+        );
+      }
+    });
+    
+    Meteor.startup(function() {
+        ReactDOM.render(<Avatar username="thekavish" />, document.getElementById('composite-example') );
     });
 }
 
